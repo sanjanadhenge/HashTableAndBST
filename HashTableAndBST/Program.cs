4c684fbb73e843e247a9df2cdfd67996ab9ccf4c
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-17 09:13:42 +0530

    [sanjana] Add : Find frequency

--- HashTableAndBST/Program.cs
+++ HashTableAndBST/Program.cs
@@ -11,6 +11,17 @@ namespace DataStruct
     {
         static void Main(string[] args)
         {
+            MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(5);
+            myMapNode.Add("0", "to");
+            myMapNode.Add("1", "be");
+            myMapNode.Add("2", "or");
+            myMapNode.Add("3", "not");
+            myMapNode.Add("4", "to");
+            myMapNode.Add("5", "be");
+            string hash5 = myMapNode.Get("5");
+            Console.WriteLine("5th hash value" + hash5);
+            string hash2 = myMapNode.Get("2");
+            Console.WriteLine("2nd hash value" + hash2);
         }
     }
 }
