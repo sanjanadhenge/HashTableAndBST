e4f133b572a937176f61471516e1cd82e8cccf51
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-17 08:59:22 +0530

    [sanjana] Add : Hash table and BST

---
+++ HashTableAndBST/MyMapNode.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndBST
+{
+   
+        public class MyMapNode<K, V>
+        {
+            private readonly int size;
+            private readonly LinkedList<KeyValue<K, V>>[] items;
+            public MyMapNode(int size)
+            {
+                this.size = size;
+                this.items = new LinkedList<KeyValue<K, V>>[size];
+            }
+            protected int GetArrayPosition(K Key)
+            {
+                int position = Key.GetHashCode() % size;
+                return Math.Abs(position);
+            }
+            public V Get(K Key)
+            {
+                int position = GetArrayPosition(Key);
+                LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+                foreach (KeyValue<K, V> item in linkedlist)
+                {
+                    if (item.Key.Equals(Key))
+                    {
+                        return item.Value;
+                    }
+                }
+                return default(V);
+            }
+            public void Add(K key, V value)
+            {
+                int position = GetArrayPosition(key);
+                LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+                KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+                linkedlist.AddLast(item);
+            }
+           
+            public void Remove(K key)
+            {
+                int position = GetArrayPosition(key);
+                LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+                bool itemfound = false;
+                KeyValue<K, V> founditem = default(KeyValue<K, V>);
+                foreach (KeyValue<K, V> item in linkedlist)
+                {
+                    if (item.Key.Equals(key))
+                    {
+                        itemfound = true;
+                        founditem = item;
+                    }
+                }
+                if (itemfound)
+                {
+                    linkedlist.Remove(founditem);
+                }
+            }
+            protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+            {
+                LinkedList<KeyValue<K, V>> linkedlist = items[position];
+                if (linkedlist == null)
+                {
+                    linkedlist = new LinkedList<KeyValue<K, V>>();
+                    items[position] = linkedlist;
+                }
+                return linkedlist;
+            }
+            public struct KeyValue<K, V>
+            {
+                public K Key { get; set; }
+                public V Value { get; set; }
+            }
+        }
+
+    
+}
---
+++ HashTableAndBST/Program.cs
@@ -0,0 +1,16 @@
+﻿using HashTableAndBST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStruct
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
