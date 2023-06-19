94ed15413fe527070d42acd5c319d3fe8277b4d5
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-19 18:57:04 +0530

    [sanjana] Add : Create BST

--- HashTableAndBST/Program.cs
+++ HashTableAndBST/Program.cs
@@ -11,27 +11,12 @@ namespace DataStruct
     {
         static void Main(string[] args)
         {
-            MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(5);
-            myMapNode.Add("0", "Paranoids");
-            myMapNode.Add("0", "are");
-            myMapNode.Add("0", "not");
-            myMapNode.Add("0", "because");
-            myMapNode.Add("0", "they");
-            myMapNode.Add("0", "are");
-            myMapNode.Add("0", "paranoid");
-            myMapNode.Add("0", "but");
-            myMapNode.Add("0","beacause");
-            myMapNode.Add("0", "they");
-            myMapNode.Add("0", "keep");
-            myMapNode.Add("0", "putting");
-            myMapNode.Add("0", "themselves");
-            myMapNode.Add("0", "deliberately");
-            myMapNode.Add("0", "into");
-            myMapNode.Add("0", "paranoid");
-            myMapNode.Add("0", "avoidable");
-            myMapNode.Add("0", "situations");
-            // myMapNode.FindFrequency("0", "paranoid");
-            myMapNode.Remove("0", "avoidable");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
+
+
         }
     }
 }
