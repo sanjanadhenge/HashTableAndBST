dbd5b189792a09a70ac81f5dc26a4cec5639d0e7
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-19 18:33:18 +0530

    [sanjana] Add : Find Frequency

--- HashTableAndBST/Program.cs
+++ HashTableAndBST/Program.cs
@@ -12,17 +12,25 @@ namespace DataStruct
         static void Main(string[] args)
         {
             MyMapNode<string, string> myMapNode = new MyMapNode<string, string>(5);
-            myMapNode.Add("0", "to");
-            myMapNode.Add("1", "be");
-            myMapNode.Add("2", "or");
-            myMapNode.Add("3", "not");
-            myMapNode.Add("0", "to");
-            myMapNode.Add("5", "be");
-            string hash5 = myMapNode.Get("5");
-            Console.WriteLine("5th hash value" + hash5);
-            string hash2 = myMapNode.Get("2");
-            Console.WriteLine("2nd hash value" + hash2);
-            myMapNode.FindFrequency("0", "to");
+            myMapNode.Add("0", "Paranoids");
+            myMapNode.Add("0", "are");
+            myMapNode.Add("0", "not");
+            myMapNode.Add("0", "because");
+            myMapNode.Add("0", "they");
+            myMapNode.Add("0", "are");
+            myMapNode.Add("0", "paranoid");
+            myMapNode.Add("0", "but");
+            myMapNode.Add("0","beacause");
+            myMapNode.Add("0", "they");
+            myMapNode.Add("0", "keep");
+            myMapNode.Add("0", "putting");
+            myMapNode.Add("0", "themselves");
+            myMapNode.Add("0", "deliberately");
+            myMapNode.Add("0", "into");
+            myMapNode.Add("0", "paranoid");
+            myMapNode.Add("0", "avoidable");
+            myMapNode.Add("0", "situations");
+            myMapNode.FindFrequency("0", "paranoid");
         }
     }
 }
